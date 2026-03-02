288569ffbfb718304acecd98c4b223d215f3d781
Eevoo <49214133+Eevoo@users.noreply.github.com>
2026-03-02 00:51:43 +0000

    rip out piclaw and just do another nanobot host

--- piclaw/dist/index.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bun
-import { handleCliOptions } from "./cli.js";
-import { main } from "./runtime.js";
-handleCliOptions();
-main().catch((err) => {
-    console.error("[piclaw] Fatal:", err);
-    process.exit(1);
-});
--- piclaw/src/agent-control.ts
+++
@@ -1,3 +0,0 @@
-export { parseControlCommand } from "./agent-control-parser.js";
-export { applyControlCommand } from "./agent-control-handlers.js";
-export type { AgentControlCommand, AgentControlResult } from "./agent-control-types.js";
